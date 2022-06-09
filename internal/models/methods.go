72ae6123bcef85806efb50692e0b10bde94f0971
azazel3ooo <vasdanser@gmail.com>
2022-06-10 00:19:12 +0300

    fix debug chfnges

--- internal/models/methods.go
+++ internal/models/methods.go
@@ -19,10 +19,10 @@ func (c *Config) Init() error {
 		return err
 	}
 	if c.ServerAddress == "" {
-		flag.StringVar(&c.ServerAddress, "a", "localhost:8081", "Server address")
+		flag.StringVar(&c.ServerAddress, "a", "localhost:8080", "Server address")
 	}
 	if c.URLBase == "" {
-		flag.StringVar(&c.URLBase, "b", "http://127.0.0.1:8081", "Base url")
+		flag.StringVar(&c.URLBase, "b", "http://127.0.0.1:8080", "Base url")
 	}
 	if c.FileStoragePath == "" {
 		flag.StringVar(&c.FileStoragePath, "f", "./tmp/tmp.txt", "Filepath for backup")
